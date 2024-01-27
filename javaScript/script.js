75dd2b265a6791cc4f45ad2fb868dd755a535d5b
calabria1 <arthurcalabriaa@gmail.com>
2024-01-26 22:20:40 -0300

    update

---
+++ javaScript/script.js
@@ -0,0 +1,29 @@
+const timeStamp = '1706273054161';
+const apiKey = '82eaa6525558b9014aa0602aebd58cf4';
+const md5 = 'ef5162c1f492c2251a0a77e2fd003e1e';
+const url = `http://gateway.marvel.com/v1/public/comics?ts=${timeStamp}&apikey=${apiKey}&hash=${md5}`;
+
+
+    fetch(url)
+    .then((response) => {
+        return response.json();
+    })
+    .then((json)=> {
+        json.data.results.forEach(element => {
+            const titulo = element.title;
+            const foto = element.thumbnail.path + '.' + element.thumbnail.extension;
+
+            let htmlContent = document.querySelector('.nome');
+            htmlContent.innerHTML  += `<div><h1>${titulo}</h1><img src="${foto}" alt="${titulo}" /><hr/></div>`;
+            
+
+            
+        
+            
+        });    
+    })
+    
+
+
+    
+
